9ba023333b87addc057a5b3664c25f75fc4f95e5
Jari Honkanen <Jari@Honkanen.cc>
2024-08-02 12:13:33 -0400

    Added server app

---
+++ server.py
@@ -0,0 +1,38 @@
+"""
+   Emotion Detector Web App using Watson Libraries
+"""
+# Imports
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+# Initiate flask app
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def sent_analyzer():
+    """ Retrieve text input from HTML page and run emotion detection function.
+        Return emotion scores.
+    """
+
+    # Retrieve text
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    # Run emotion detection
+    emotions = emotion_detector(text_to_analyze)
+
+    resp_str = f"For the given statement, the system response is " \
+            f"'anger': {emotions['anger']}, 'disgust': {emotions['disgust']}, 'fear': {emotions['fear']}, " \
+            f"'joy': {emotions['joy']} and 'sadness': {emotions['sadness']}. " \
+            f"The dominant emotion is {emotions['dominant_emotion']}."
+
+    return resp_str
+
+@app.route("/")
+def render_index_page():
+    """ Render main Web app page 
+    """
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    # Run Web App
+    app.run(host="0.0.0.0", port=5000)
